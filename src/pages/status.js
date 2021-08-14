cbfaf510099ba5b64deb4dde5a44ac105c404983
Nikhil Kuyya <nikhilkuyya@gmail.com>
2021-08-14 13:01:25 +0530

    add custom page

--- src/pages/status.js
+++ src/pages/status.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Layout from '@lekoarts/gatsby-theme-minimal-blog/src/components/layout'
 
 export default function StatusPage(){
-    return <pre>{JSON.stringify({},null,2)}</pre>
+    return (<Layout>
+        <h2>Status</h2>
+    </Layout>);
 }
